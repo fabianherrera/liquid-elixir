345646e4dbfec4790ff0f96f7fa0b17cb24cfb88
Antonio Abella <antonioabella@designiostecnologicos.com>
2018-05-24 20:55:13 -0400

    fix(Capture): use the new Tag

--- lib/liquid/combinators/tags/capture.ex
+++ lib/liquid/combinators/tags/capture.ex
@@ -1,50 +1,24 @@
 defmodule Liquid.Combinators.Tags.Capture do
   @moduledoc """
-  Allows you to leave un-rendered code inside a Liquid template.
-  Any text within the opening and closing comment blocks will not be output,
-  and any Liquid code within will not be executed
-  Input:
+  Stores the result of a block into a variable without rendering it in place.
   ```
-    Anything you put between {% comment %} and {% endcomment %} tags
-    is turned into a comment.
+    {% capture heading %}
+      Monkeys!
+    {% endcapture %}
+    ...
+    <h1>{{ heading }}</h1> <!-- then you can use the `heading` variable -->
   ```
-  Output:
-  ```
-    Anything you put between  tags
-    is turned into a comment
-  ```
+  Capture is useful for saving content for use later in your template, such as in a sidebar or footer.
   """
+
   import NimbleParsec
-
-  def capture_sentences do
-    empty()
-    |> optional(parsec(:__parse__))
-    |> tag(:capture_sentences)
-  end
-
-  @doc "Open capture tag: {% capture my_val %}"
-  def open_tag do
-    empty()
-    |> parsec(:start_tag)
-    |> ignore(string("capture"))
-    |> concat(parsec(:variable_name))
-    |> concat(parsec(:end_tag))
-  end
-
-  @doc "Close capture tag: {% endcapture %}"
-  def close_tag do
-    empty()
-    |> parsec(:start_tag)
-    |> ignore(string("endcapture"))
-    |> concat(parsec(:end_tag))
-  end
+  alias Liquid.Combinators.Tag
 
   def tag do
-    empty()
-    |> parsec(:open_tag_capture)
-    |> optional(parsec(:capture_sentences))
-    |> parsec(:close_tag_capture)
-    |> tag(:capture)
-    |> optional(parsec(:__parse__))
+    Tag.define("capture",
+      fn combinator -> parsec(combinator, :variable_name) end,
+      "endcapture",
+      fn combinator -> optional(combinator, parsec(:__parse__) |> tag(:capture_sentences)) end
+    )
   end
 end
