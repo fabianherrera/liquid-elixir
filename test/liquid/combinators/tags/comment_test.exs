ab61fe57d36bc07edefcb8a4d92742c859f6bf85
Antonio Abella <antonioabella@designiostecnologicos.com>
2018-05-31 20:56:00 -0400

    refactor(Comment): use functions instead of parsec

--- test/liquid/combinators/tags/comment_test.exs
+++ test/liquid/combinators/tags/comment_test.exs
@@ -10,7 +10,7 @@ defmodule Liquid.Combinators.Tags.CommentTest do
       &Parser.comment/1,
       [
         comment: [
-          comment_body: [" Allows you to leave un-rendered code inside a Liquid template. Any text within the opening and closing comment blocks will not be output, and any Liquid code within will not be executed. "]
+          comment_content: [" Allows you to leave un-rendered code inside a Liquid template. Any text within the opening and closing comment blocks will not be output, and any Liquid code within will not be executed. "]
         ]
       ]
     )
