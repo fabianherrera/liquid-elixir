1f9bf326794fa338d886c43bd76f2e6705c061f8
ingsissam <33227372+ingsissam@users.noreply.github.com>
2018-06-02 15:33:28 -0400

    fix: Liquid variable renders

--- lib/liquid/combinators/translators/liquid_variable.ex
+++ lib/liquid/combinators/translators/liquid_variable.ex
@@ -1,41 +1,47 @@
 defmodule Liquid.Combinators.Translators.LiquidVariable do
   alias Liquid.Combinators.Translators.General
 
-  def translate(markup) do
-    literal = hd(markup)
-
-    if is_tuple(literal) do
-      {key, value} = literal
-      literal_have_filters = Enum.any?(value, fn x -> have_filters(x) end)
-
-      if literal_have_filters == true do
-        variable_list = literal |> elem(1)
-        variable_name = variable_list |> hd
-        filters_list = Enum.filter(variable_list, fn x -> is_tuple(x) == true end)
-        filters = transform_filters(filters_list)
-        %Liquid.Variable{name: variable_name, parts: [variable_name], filters: filters}
-      else
-        variable_list = literal |> elem(1)
-        filters = transform_filters(markup)
-        parts = Enum.map(variable_list, &General.variable_in_parts/1)
-        name = General.variable_to_string(parts)
-        %Liquid.Variable{name: name, parts: parts, filters: filters}
-      end
-    else
-      if is_number(literal) or is_boolean(literal) do
-        variable_name = "#{literal}"
-      else
-        variable_name = "'#{literal}'"
-      end
-
-      filters_list = Enum.filter(markup, fn x -> is_tuple(x) == true end)
-      filters = transform_filters(filters_list)
-      %Liquid.Variable{name: variable_name, literal: literal, filters: filters}
-    end
+  def translate(variable: [variable_parts: variable_list]) do
+    variable_parts = Enum.map(variable_list, &General.variable_in_parts/1)
+    variable_name = General.variable_to_string(variable_parts)
+    %Liquid.Variable{name: variable_name, parts: variable_parts}
   end
 
-  defp have_filters({value, _}), do: value == :filter
-  defp have_filters(_), do: false
+  def translate(variable: [variable_parts: variable_list, filters: filters]) do
+    variable_parts = Enum.map(variable_list, &General.variable_in_parts/1)
+    variable_name = General.variable_to_string(variable_parts)
+    filters_markup = transform_filters(filters)
+    %Liquid.Variable{name: variable_name, parts: variable_parts, filters: filters_markup}
+  end
+
+  def translate([value, filters: filters]) do
+    case is_bitstring(value) do
+      true ->
+        name = "'#{value}'"
+        literal = "#{value}"
+
+      false ->
+        name = "#{value}"
+        literal = value
+    end
+
+    filters_markup = transform_filters(filters)
+    %Liquid.Variable{name: name, filters: filters_markup, literal: literal}
+  end
+
+  def translate([value]) do
+    case is_bitstring(value) do
+      true ->
+        name = "'#{value}'"
+        literal = "#{value}"
+
+      false ->
+        name = "#{value}"
+        literal = value
+    end
+
+    %Liquid.Variable{name: name, literal: literal}
+  end
 
   defp transform_filters(filters_list) do
     Keyword.get_values(filters_list, :filter)
@@ -48,7 +54,23 @@ defmodule Liquid.Combinators.Translators.LiquidVariable do
 
   defp filters_to_list({filter_name, filter_param}) do
     filter_param_value = filter_param |> elem(1)
-    value = Keyword.get(filter_param_value, :value)
-    [String.to_atom(filter_name), ["#{value}"]]
+    value = Keyword.get_values(filter_param_value, :value)
+
+    if length(value) > 1 do
+      filter_param_value = Enum.map(value, fn x -> to_string(x) end)
+      [String.to_atom(filter_name), filter_param_value]
+    else
+      [filter_param_value] = value
+
+      case is_bitstring(filter_param_value) do
+        true ->
+          param = "'#{filter_param_value}'"
+
+        false ->
+          param = "#{filter_param_value}"
+      end
+
+      [String.to_atom(filter_name), [param]]
+    end
   end
 end
