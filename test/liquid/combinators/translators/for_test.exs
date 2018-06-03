237b05097120ac43f10e9ba3ab9b8e61093c4d87
fabianherrera@gmail.com <admnts9000!>
2018-06-03 19:23:05 -0400

    Merge remote-tracking branch 'upstream/WIP' into WIP
    # Conflicts:
    #	test/liquid/combinators/translators/if_test.exs

--- test/liquid/combinators/translators/for_test.exs
+++ test/liquid/combinators/translators/for_test.exs
@@ -10,7 +10,7 @@ defmodule Liquid.Combinators.Translators.ForTest do
       "{%for item in repeat_array%}{%ifchanged%}{{item}}{% endifchanged %}{%endfor%}",
       "{%for item in (1..3)%}{%ifchanged%}{{item}}{%for item in (4..6)%}{{item}}{%endfor%}{% endifchanged %}{%endfor%}",
       "0{% for i in (1..3) %} {{ i }}{% endfor %}",
-#      "0{%\nfor i in (1..3)\n%} {{\ni\n}}{%\nendfor\n%}"
+      "0{%\nfor i in (1..3)\n%} {{\ni\n}}{%\nendfor\n%}"
     ]
     |> Enum.each(fn tag ->
       test_ast_translation(tag, params)
