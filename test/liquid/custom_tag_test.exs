f33eb18b457eecd0ee1f5844ee1a93463ee0f558
ingsissam <33227372+ingsissam@users.noreply.github.com>
2018-08-23 11:47:56 -0400

    feat: Renders error when the customs tags is not registered

--- test/liquid/custom_tag_test.exs
+++ test/liquid/custom_tag_test.exs
@@ -5,18 +5,22 @@ defmodule Liquid.CustomTagTest do
   alias Liquid.{Template, Tag}
 
   defmodule MinusOneTag do
-    def parse(%Tag{} = tag, %Template{} = context) do
-      {tag, context}
-    end
-
     def render(output, tag, context) do
       number = tag.markup |> Integer.parse() |> elem(0)
       {["#{number - 1}"] ++ output, context}
     end
   end
 
+  defmodule PlusOneTag do
+    def render(output, tag, context) do
+      number = tag.markup |> Integer.parse() |> elem(0)
+      {["#{number + 1}"] ++ output, context}
+    end
+  end
+
   setup_all do
-    Liquid.Registers.register("culo", MinusOneTag, Tag)
+    Liquid.Registers.register("MinusOne", MinusOneTag, Tag)
+    Liquid.Registers.register("PlusOne", PlusOneTag, Tag)
     Liquid.start()
     on_exit(fn -> Liquid.stop() end)
     :ok
@@ -24,8 +28,23 @@ defmodule Liquid.CustomTagTest do
 
   test "custom tag from example(almost random now :)" do
     assert_template_result("123", "123{% assign qwe = 5 %}")
-    assert_template_result("4", "{% culo 5 %}")
-    assert_template_result("a1b", "a{% culo 2 %}b")
+    assert_template_result("4", "{% MinusOne 5 %}")
+    assert_template_result("a1b", "a{% MinusOne 2 %}b")
+  end
+
+  test "more than one custom tag" do
+    assert_template_result("43", "{% assign qwe = 5 %}{% MinusOne 5 %}{% PlusOne 2 %}")
+  end
+
+  test "custom tag error" do
+    assert_raise Liquid.SyntaxError,
+                 "This custom tag: {% not_registered %} is not registered",
+                 fn ->
+                   tag =
+                     "{% assign qwe = 5 %}{% MinusOne 5 %}{% PlusOne 2 %}{% not_registered hola%}"
+
+                   Template.parse(tag) |> Template.render()
+                 end
   end
 
   defp assert_template_result(expected, markup, assigns \\ %{}) do
