4a82ff3e9741a3a028d68792b548cbd29857142f
Antonio Abella <antonioabella@gmail.com>
2018-09-26 19:36:08 -0500

    fix: translator unless

--- lib/liquid/translators/tags/if.ex
+++ lib/liquid/translators/tags/if.ex
@@ -9,41 +9,45 @@ defmodule Liquid.Translators.Tags.If do
   @doc """
   Takes the markup of the new AST, creates a `Liquid.Block` struct (old AST) and fill the keys needed to render a If tag.
   """
-  @spec translate(If.conditional_body()) :: Block.t()
-  def translate(conditions: [value], body: body) when is_bitstring(value) do
-    create_block_if("\"#{Markup.literal(value)}\"", body)
+  @spec translate(atom(), If.conditional_body()) :: Block.t()
+  def translate(name, conditions: [value], body: body) when is_bitstring(value) do
+    create_block_if(name, "\"#{Markup.literal(value)}\"", body)
   end
 
-  def translate([{:conditions, [value]}, {:body, body_parts} | elselist]) when is_bitstring(value) do
-    create_block_if("\"#{Markup.literal(value)}\"", body_parts, normalize_elselist(elselist))
+  def translate(name, [{:conditions, [value]}, {:body, body_parts} | elselist]) when is_bitstring(value) do
+    create_block_if(name, "\"#{Markup.literal(value)}\"", body_parts, normalize_elselist(elselist))
   end
 
-  def translate(conditions: conditions, body: body) do
-    create_block_if("#{Markup.literal(conditions)}", body)
+  def translate(name, conditions: conditions, body: body) do
+    create_block_if(name, "#{Markup.literal(conditions)}", body)
   end
 
-  def translate([{:conditions, conditions}, {:body, body_parts} | elselist]) do
-    create_block_if("#{Markup.literal(conditions)}", body_parts, normalize_elselist(elselist))
+  def translate(name, [{:conditions, conditions}, {:body, body_parts} | elselist]) do
+    create_block_if(name, "#{Markup.literal(conditions)}", body_parts, normalize_elselist(elselist))
   end
 
   defp normalize_elselist([{:elsif, _} | [_]] = else_list) do
-    {list, _} = Enum.reduce_while(else_list, {[], nil}, fn x, {list, last} ->
-      if {:elsif, _} = x do
-        {:cont, {[x | list], x}}
-      else
-        {tag, params} = last
-        final_list = List.replace_at(list, length(list), {tag, params ++ x})
-        {:halt, {final_list, nil}}
-      end
-    end)
+    {list, _} =
+      Enum.reduce_while(else_list, {[], nil}, fn x, {list, last} ->
+        case x do
+          {:elsif, _} ->
+            {:cont, {[x | list], x}}
+
+          _ ->
+            {tag, params} = last
+            final_list = List.replace_at(list, length(list) - 1, {tag, Enum.reverse([x | Enum.reverse(params)])})
+            {:halt, {final_list, nil}}
+        end
+      end)
+
     list
   end
 
   defp normalize_elselist(else_list), do: else_list
 
-  defp create_block_if(markup, nodelist) do
+  defp create_block_if(name, markup, nodelist) do
     block = %Liquid.Block{
-      name: :if,
+      name: name,
       markup: markup,
       nodelist: nodelist |> NimbleTranslator.process_node() |> General.types_only_list(),
       # General.types_only_list(NimbleTranslator.process_node(nodelist)),
@@ -53,9 +57,9 @@ defmodule Liquid.Translators.Tags.If do
     IfElse.parse_conditions(block)
   end
 
-  defp create_block_if(markup, nodelist, else_list) do
+  defp create_block_if(name, markup, nodelist, else_list) do
     block = %Liquid.Block{
-      name: :if,
+      name: name,
       markup: markup,
       nodelist: nodelist |> NimbleTranslator.process_node() |> General.types_only_list(),
       blank: Blank.blank?(nodelist) and Blank.blank?(else_list),
